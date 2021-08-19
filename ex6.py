ef5b5b42b05ee2497efce6b8b1f9bc43b8a59716
Chemistrier <45254476+Chemistrier@users.noreply.github.com>
2021-08-19 14:42:04 +0800

    Create ex6.py
    adding excercise 6

---
+++ ex6.py
@@ -0,0 +1,22 @@
+types_of_people = 10 #set the variable
+x = f"There are {types_of_people} types of people."#set x as a f-string with types_of_people
+
+binary = "binary"#set binary as binary
+do_not = "don't"#set do_not as don't
+y = f"Those who know {binary} and those who {do_not}."#y set y as a f-string with binary and do_not
+
+print(x)#print out x
+print(y)#print out y
+
+print(f"I said: {x}")#use f string to print out I said: x
+print(f"I also said: '{y}'")# use f string to print out I said: y
+
+hilarious = False#set hilarious as false
+joke_evaluation = "Isn't that joke so funny?! {}"#set joke_evaluation
+
+print(joke_evaluation.format(hilarious))#print joke_evaluation as false
+
+w = "This is the left side of..."#set w string
+e = "a string with a right side."# set e string
+
+print(w + e)#combine two strings 
