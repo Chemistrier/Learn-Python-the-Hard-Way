b648c874603f8f43999423a8e380a6714e8e5050
Chemistrier <45254476+Chemistrier@users.noreply.github.com>
2021-08-23 20:07:17 +0800

    Update ex3-3.py
    i forget the , 0-0

--- ex3-3.py
+++ ex3-3.py
@@ -1,2 +1,2 @@
-print("I want to calculuate this:" 22+250)
-print("I also want to calculuate this: "123571*2333)
+print("I want to calculuate this: ", 22+250)
+print("I also want to calculuate this: ",123571*2333)
