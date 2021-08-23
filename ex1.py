656e21ebd50c1e842232d84a11dc85cfb88051fa
Chemistrier <45254476+Chemistrier@users.noreply.github.com>
2021-08-23 20:12:43 +0800

    hi
    complete the ex1 study drill

--- ex1.py
+++ ex1.py
@@ -1,7 +1,7 @@
 print("Hello World!")
 print("Hello Again.")
 print("I like typing this.")
-#print("This is fun.")
+#print("This is fun."), it makes the line a comment and will not run by the terminal 
 print("Yay! Printing.")
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
