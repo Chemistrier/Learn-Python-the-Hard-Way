8c1d7519b2d9b753a33f996880c5cde0dc1f4226
Chemistrier <45254476+Chemistrier@users.noreply.github.com>
2021-08-23 19:37:58 +0800

    Update ex7.py
    complete ex7 study drill

--- ex7.py
+++ ex7.py
@@ -1,20 +1,20 @@
 
-print("Mary had a little lamb.")
-print("Its fleece was white as {}.".format('snow'))
-print("And everywhere that Mary went.")
-print("." * 10)  # what'd that do?
+print("Mary had a little lamb.")#print mary had a little lamb
+print("Its fleece was white as {}.".format('snow'))#put snow in the {} that is the quote
+print("And everywhere that Mary went.") #print and everywhere that Mary went
+print("." * 10)  # repeat printing . ten times
 
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
+end1 = "C" #end1 is c
+end2 = "h" #end2 is h
+end3 = "e" #end3 is e
+end4 = "e" #end4 is e
+end5 = "s" #end5 is s
+end6 = "e" #end6 is e
+end7 = "B" #end7 is B
+end8 = "u" #end8 is u
+end9 = "r" #end9 is r
+end10 = "g" #end10 is g
+end11 = "e" #end11 is e
+end12 = "r" #end12 is r
 
-print(end1 + end2 + end3 + end4 + end5 + end6 + " "+end7 + end8 + end9 + end10 + end11 + end12)
+print(end1 + end2 + end3 + end4 + end5 + end6 + " "+end7 + end8 + end9 + end10 + end11 + end12)# combine end1 to end12 to for cheeseburger string
