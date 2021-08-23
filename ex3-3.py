94b57df16848034635727110e480fcf636b1e7ef
Chemistrier <45254476+Chemistrier@users.noreply.github.com>
2021-08-23 20:05:59 +0800

    hi
    finish ex3 study drill

---
+++ ex3-3.py
@@ -0,0 +1,2 @@
+print("I want to calculuate this:" 22+250)
+print("I also want to calculuate this: "123571*2333)
--- ex3.py
+++ ex3.py
@@ -1,21 +1,21 @@
-print("I will now count my chickens:  ")
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
-print(" ")
-print("Now I will count the eggs:")
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print("I will now count my chickens:  ")#print out I will now count my schikens
 
-print("Is it true that 3 + 2 < 5 - 7?")
+print("Hens", 25.0 + 30.0 / 6.0)# print out Hens and count the number 30
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)#print out Roosters and its amount
+print("Now I will count the eggs:")#print now I will count the eggs
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)# do the math and come out 6.75
 
-print(3 + 2 < 5 - 7)
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")#print Is it true that 3 + 2 < 5 - 7?
 
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7?", 5 - 7)
+print(3.0 + 2.0 < 5.0 - 7.0)# print whether 3 + 2 < 5 - 7 with boolean variable
 
-print("Oh, that's why it's False.")
+print("What is 3.0 + 2.0?", 3.0 + 2.0)#print out what is 3+2 and do the math
+print("What is 5.0 - 7.0?", 5.0 - 7.0)#print out what is 5-7 and do the math
 
-print("How about some more.")
+print("Oh, that's why it's False.")#print out Oh, that's why it's False.
 
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+print("How about some more.")#print out How about some more.
+
+print("Is it greater?", 5.0 > -2.0) #print out is it greater? and see whether 5 is greater than -2
+print("Is it greater or equal?", 5.0 >= -2.0)# print Is it greater or equal? and see whether 5 is greater or equal to -2
+print("Is it less or equal?", 5.0 <= -2.0)#print is it less or equal and see whether 5 is less or equal to -2
