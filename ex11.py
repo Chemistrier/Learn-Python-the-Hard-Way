e886bb97407dccd731ab2192224b1b70076b4f31
Chemistrier <45254476+Chemistrier@users.noreply.github.com>
2021-08-23 14:32:01 +0800

    Update ex11.py
    finish the study drill

--- ex11.py
+++ ex11.py
@@ -4,5 +4,9 @@ print("How tall are you?"),
 height = input()
 print("How much are your weight"),
 weight = input()
+place = input("Where do you live")
 
-print("So, you're %r old, %r tall, and %r heavy"%(age, height, weight))
+print("So, you're %r old, %r tall, %r heav, live in %r"%(age, height, weight, place))
+#input things to the terminal
+#name = input("what is your name ")
+#because their are " in the answer 
