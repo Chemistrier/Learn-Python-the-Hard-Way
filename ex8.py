1531eae4c76b92c1fd44c073577758fadb287671
Chemistrier <45254476+Chemistrier@users.noreply.github.com>
2021-08-23 19:40:41 +0800

    Update ex8.py
    finish ex8 study drill

--- ex8.py
+++ ex8.py
@@ -1,12 +1,12 @@
-formatter = "{} {} {} {}"
+formatter = "{} {} {} {}"#create a sequence with four slots
 
-print(formatter.format(1, 2, 3, 4))
-print(formatter.format("one", "two", "three", "four"))
-print(formatter.format(True, False, False, True))
-print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(1, 2, 3, 4))# input 4 integers
+print(formatter.format("one", "two", "three", "four"))#input 4 words
+print(formatter.format(True, False, False, True))#input 4 boolean value
+print(formatter.format(formatter, formatter, formatter, formatter))#input 4 slots sequence in side every slots, which create 16 slots
 print(formatter.format(
     "I am the bone of my sword",
     "Unknown to death",
     "nor to life",
     "heart is glass"
-))
+))#input 4 lines of string 
