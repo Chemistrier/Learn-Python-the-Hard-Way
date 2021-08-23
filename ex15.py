74737d6ee54a8656c336ff96b376cc8205098227
Chemistrier <45254476+Chemistrier@users.noreply.github.com>
2021-08-23 20:55:32 +0800

    Update ex15.py
    finish ex15 study drill

--- ex15.py
+++ ex15.py
@@ -1,12 +1,15 @@
-from sys import argv
+from sys import argv #import argv from sys
 
-script, filename = argv
+script, filename = argv # assign two arguments
 
-txt = open(filename)
-
-print("Here's your file %r: "%(filename))
-print(txt.read())
-print("type filename again:")
-file_again = input("> ")
-txt_again = open(file_again)
-print(txt_again.read())
+txt = open(filename) # open the file that has the filename and store it into txt
+print("Here's your file %r: "%(filename)) #print out the filename
+print(txt.read())#read the text stored in txt
+txt.close()
+print("type filename again:")#ask the user to type the filename again
+file_again = input("> ")#take the filename again
+txt_again = open(file_again)#ppen the file again using file_again
+print(txt_again.read())#print out the things stored in txt_again
+txt_again.close()
+#the content in the file will not print twice
+#using input() will ask the filename after the program start running, but using arguments will use the inputted value directly, input() will be convenient if you want to tell the user to input the way you want and arguments is simply faster to running
