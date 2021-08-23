69c34d78385131a45c12d7ca6ed4e0073a4769b4
Chemistrier <45254476+Chemistrier@users.noreply.github.com>
2021-08-23 19:43:30 +0800

    Update ex6.py
    complete ex6 study drill

--- ex6.py
+++ ex6.py
@@ -8,8 +8,8 @@ y = f"Those who know {binary} and those who {do_not}."#y set y as a f-string wit
 print(x)#print out x
 print(y)#print out y
 
-print(f"I said: {x}")#use f string to print out I said: x
-print(f"I also said: '{y}'")# use f string to print out I said: y
+print(f"I said: {x}")#use f string to print out I said: x/ string put in string
+print(f"I also said: '{y}'")# use f string to print out I said: y/ string put in string
 
 hilarious = False#set hilarious as false
 joke_evaluation = "Isn't that joke so funny?! {}"#set joke_evaluation
@@ -19,4 +19,6 @@ print(joke_evaluation.format(hilarious))#print joke_evaluation as false
 w = "This is the left side of..."#set w string
 e = "a string with a right side."# set e string
 
-print(w + e)#combine two strings 
+print(w + e)#combine two strings
+
+#adding w and e combines the two strings 
